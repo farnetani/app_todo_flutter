aa357659d1119acbfdf0b6f9ba57b89f8ffb6b6d
Arlei F. Farnetani Junior <farnetani@gmail.com>
2024-01-21 16:30:18 -0300

    scroll

--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -8,7 +8,11 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
-  TextEditingController _textEditingController = TextEditingController();
+  _TodoListPageState();
+  final TextEditingController _textEditingController = TextEditingController();
+  List<String> tarefas = [];
+  int _selectedIndex = -1; // -1 significa que nenhum item está selecionado
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +27,55 @@ class _TodoListPageState extends State<TodoListPage> {
             TextField(
               controller: _textEditingController,
             ),
+            Container(
+              color: Colors.white,
+              height: 300,
+              child: Container(
+                child: SizedBox(
+                  height: 400,
+                  child: ListView.separated(
+                      controller: _scrollController,
+                      separatorBuilder: (context, index) => const Divider(),
+                      itemCount: tarefas.length,
+                      itemBuilder: (
+                        contexto,
+                        index,
+                      ) {
+                        return Container(
+                          color: _selectedIndex == index ? Colors.blue : null,
+                          child: ListTile(
+                            title: Text(
+                              tarefas[index],
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onTap: () {
+                              setState(() {
+                                _selectedIndex =
+                                    index; // Atualiza o índice selecionado
+                              });
+                            },
+                            onLongPress: () {
+                              setState(() {
+                                tarefas.removeAt(index);
+                              });
+                            },
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                setState(() {
+                                  tarefas.removeAt(index);
+                                });
+                              },
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+              ),
+            )
           ],
         ),
       ),
@@ -30,10 +83,22 @@ class _TodoListPageState extends State<TodoListPage> {
         backgroundColor: Colors.green,
         shape: const CircleBorder(),
         onPressed: () {
-          print(_textEditingController.text);
+          if (_textEditingController.text.isNotEmpty) {
+            setState(() {
+              tarefas.add(_textEditingController.text);
+            });
+            _textEditingController.clear();
+          }
+          print(tarefas);
         },
         child: const Icon(Icons.add, color: Colors.white),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
